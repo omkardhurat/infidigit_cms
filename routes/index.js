e35317c0c17c33171b5fdad8971fc52a74eb9f02
poojashankar0305 <poojaarisella@gmail.com>
2024-05-17 03:49:00 -0700

    updates UI only

--- routes/index.js
+++ routes/index.js
@@ -20,12 +20,32 @@ router.get('/', async function(req, res, next) {
 });
 
 /* Login */
-router.post('/login', function(req, res, next) {
+router.post('/dashboard', function(req, res, next) {
   console.log("reqBody::"+JSON.stringify(req.body))
   let reqBody = req.body;
   let username = reqBody.username;
   let password = reqBody.password;
   res.render('dashboard/userDashboard', { title: 'CMS Application' });
+});
+
+router.get('/channel', function(req, res, next) {
+  res.render('user/channel', { title: 'CMS Application' });
+
+});
+
+router.get('/network', function(req, res, next) {
+  res.render('user/network', { title: 'CMS Application' });
+
+});
+
+router.get('/compaign', function(req, res, next) {
+  res.render('user/compaign', { title: 'CMS Application' });
+
+});
+
+router.get('/dashboard', function(req, res, next) {
+  res.render('dashboard/userDashboard', { title: 'CMS Application' });
+
 });
 
 module.exports = router;
